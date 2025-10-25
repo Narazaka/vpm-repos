92b25070d96d0bbdfd6b0255bd8040c9c9df8949
Narazaka <info@narazaka.net>
2025-10-25 18:28:54 +0900

    AMCのVRCSDK3.9.1-beta.1互換性をあとづけ

---
+++ scripts/fix-listing.ts
@@ -0,0 +1,15 @@
+import fs from 'fs/promises';
+import path from 'path';
+import semver from 'semver';
+
+const root = path.dirname(import.meta.dirname);
+const listingJsonPath = path.join(root, 'index.json');
+const listingJson = await fs.readFile(listingJsonPath, 'utf-8');
+const listing = JSON.parse(listingJson);
+const amc = listing.packages["net.narazaka.vrchat.avatar-menu-creater-for-ma"]
+for (const version of Object.keys(amc.versions)) {
+  if (semver.gte(version, '1.36.0')) continue;
+  const ver = amc.versions[version];
+  ver.vpmDependencies["com.vrchat.avatars"] = ">=3.0.9 <3.9.1-a";
+}
+await fs.writeFile(listingJsonPath, JSON.stringify(listing), 'utf-8');
