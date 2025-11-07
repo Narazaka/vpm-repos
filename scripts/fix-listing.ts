eb92b9bf18bb8a8878907e1ef389ff1f833d6c4a
Narazaka <info@narazaka.net>
2025-11-07 16:20:38 +0900

    allow 3.9.1-beta.2

--- scripts/fix-listing.ts
+++ scripts/fix-listing.ts
@@ -10,6 +10,6 @@ const amc = listing.packages["net.narazaka.vrchat.avatar-menu-creater-for-ma"]
 for (const version of Object.keys(amc.versions)) {
   if (semver.gte(version, '1.36.0')) continue;
   const ver = amc.versions[version];
-  ver.vpmDependencies["com.vrchat.avatars"] = ">=3.0.9 <3.9.1-a";
+  ver.vpmDependencies["com.vrchat.avatars"] = ">=3.0.9 <3.9.1-beta.1 || >=3.9.1-beta.2";
 }
 await fs.writeFile(listingJsonPath, JSON.stringify(listing), 'utf-8');
